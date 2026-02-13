35806735d27e89a34960fce8aae53574717e3c3c
Christoph Roth <189434005+RothFHmas@users.noreply.github.com>
2026-02-13 11:44:39 +0100

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Fast Docker build script with BuildKit caching enabled
-
-# Enable Docker BuildKit for faster builds with cache mounts
-export DOCKER_BUILDKIT=1
-export COMPOSE_DOCKER_CLI_BUILD=1
-
-echo "Building with Docker BuildKit enabled (faster builds with cache)..."
-docker-compose build "$@"
